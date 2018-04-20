80ffc48a5cd3f44aeeeb4fc3a17ccf6c35352886
Muhammad Firman <firman1367@gmail.com>
2018-04-20 18:55:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <head>
         <title>Monitoring Driver</title>
 		<meta http-equiv="refresh" content="300" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <!-- Bootstrap 3.3.5 -->
+        <!-- Bootstrap 3.3.5 Gan -->
 	    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 	    <!-- Font Awesome -->
 	    <link rel="stylesheet" href="bootstrap/css/font-awesome.min.css">
